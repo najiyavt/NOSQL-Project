fde34a445b8a31d70b17933b2fe8f02cf48b57f6
najiyavt <najiyavtibrahim@gmail.com>
2024-07-04 17:21:32 +0530

    Creating product

--- util/database.js
+++ util/database.js
@@ -1,17 +1,33 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
+require('dotenv').config();
+
+let _db ;
+
+const host = process.env.MONGODB_HOST ;
+const port = process.env.MONGODB_PORT ;
+const dbName = process.env.MONGODB_DB ;
+
+const uri = `mongodb://${host}:${port}/${dbName}`;
 
 const mongoConnect = callback => {
-  MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
-  )
+  MongoClient.connect(uri)
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if(_db){
+    return _db;
+  }
+  throw 'No database found!';
+}
+
+module.exports = { mongoConnect , getDb };
