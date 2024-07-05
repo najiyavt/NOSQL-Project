f65c36b45fc3717fa23b087de48a8e71b099c94b
najiyavt <najiyavtibrahim@gmail.com>
2024-07-05 22:06:04 +0530

    Getting and displaying orders

--- models/user.js
+++ models/user.js
@@ -48,7 +48,14 @@ userSchema.methods.removeFromCart = function(productId) {
   });
   this.cart.items = updatedCartItems;
   return this.save();
-}
+};
+
+userSchema.methods.clearCart = function() {
+  this.cart = { items: [] };
+  return this.save();
+};
+
+
 module.exports = mongoose.model("User" , userSchema);
 
 
