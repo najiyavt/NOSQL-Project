04949a265f52b2deaaf9acd17592bcc4bfdd478e
najiyavt <najiyavtibrahim@gmail.com>
2024-07-05 21:53:24 +0530

    Creating and getting order models

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -80,8 +81,22 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
-  req.user.addOrders()
+  req.user
+  .populate('cart.items.productId')
+    .then(user => {
+      console.log(user.cart.items);
+      const products = user.cart.items.map(i => {
+        return { quantity : i.quantity , product: {...i.productId._doc} };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products:products
+      });
+      return order.save();
+    })
     .then(result => {
       res.redirect('/orders');
     })
