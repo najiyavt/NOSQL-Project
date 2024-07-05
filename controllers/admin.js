047f980b6a185e57936e69e940c8470de7509819
najiyavt <najiyavtibrahim@gmail.com>
2024-07-05 17:56:21 +0530

    Updating products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,4 @@
+const product = require('../models/product');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -29,7 +30,6 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findById(prodId)
-    // Product.findById(prodId)
     .then(product => {
       if (!product) {
         return res.redirect('/');
@@ -51,16 +51,13 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
   
-  const product = new Product(
-    updatedTitle,
-    updatedImageUrl,
-    updatedPrice,
-    updatedDesc , 
-    prodId
-  );
-      
-  product
-  .save()
+  Product.findById(prodId).then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl=updatedImageUrl;
+    product.description=updatedDesc;
+    return product.save()
+  })
   .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
@@ -69,7 +66,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
     .then(products => {
       res.render('admin/products', {
         prods: products,
